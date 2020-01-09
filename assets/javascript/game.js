14cdafb1641e7f0fdffc4f8001d19eebe10637dc
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-08 21:00:49 -0800

    started HTML and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Create an array that lists out all of the computer choices
+computerChoice = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+//VARIABLES
+//===============================================================================
+
+//Create variables to hold number of wins, losses, guesses left, guesses so far
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+
+// FUNCTIONS
+// ==============================================================================
