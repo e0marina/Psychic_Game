e0cb2d34deeb2d398447848f3753bfce1c6e1ea7
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-12 10:51:51 -0800

    added all switch statements for alpha char

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -46,8 +46,8 @@ var alreadyGuessedText = document.getElementById("already-guessed");
 
 // FUNCTIONS
 // ==============================================================================
-// ?initialize the game for first time (comes in handy when need to reset guess counter), place values that need to reset inside
-//listens for enter (13) can't be inside another func
+
+//listens for alpha key presses, since we are only interested in alpha characters
 document.addEventListener("keypress", function(event) {
   //console.log(document.getElementById("already-guessed"));
   switch (event.keyCode) {
@@ -63,9 +63,97 @@ document.addEventListener("keypress", function(event) {
       console.log("c");
       keyPressFunction("c");
       break;
-    case 99:
-      console.log("c");
-      keyPressFunction("c");
+    case 100:
+      console.log("d");
+      keyPressFunction("d");
+      break;
+    case 101:
+      console.log("e");
+      keyPressFunction("e");
+      break;
+    case 102:
+      console.log("f");
+      keyPressFunction("f");
+      break;
+    case 103:
+      console.log("g");
+      keyPressFunction("g");
+      break;
+    case 104:
+      console.log("h");
+      keyPressFunction("h");
+      break;
+    case 105:
+      console.log("i");
+      keyPressFunction("i");
+      break;
+    case 106:
+      console.log("j");
+      keyPressFunction("j");
+      break;
+    case 107:
+      console.log("k");
+      keyPressFunction("k");
+      break;
+    case 108:
+      console.log("l");
+      keyPressFunction("l");
+      break;
+    case 109:
+      console.log("m");
+      keyPressFunction("m");
+      break;
+    case 110:
+      console.log("n");
+      keyPressFunction("n");
+      break;
+    case 111:
+      console.log("o");
+      keyPressFunction("o");
+      break;
+    case 112:
+      console.log("p");
+      keyPressFunction("p");
+      break;
+    case 113:
+      console.log("q");
+      keyPressFunction("q");
+      break;
+    case 114:
+      console.log("r");
+      keyPressFunction("r");
+      break;
+    case 115:
+      console.log("s");
+      keyPressFunction("s");
+      break;
+    case 116:
+      console.log("t");
+      keyPressFunction("t");
+      break;
+    case 117:
+      console.log("u");
+      keyPressFunction("u");
+      break;
+    case 118:
+      console.log("v");
+      keyPressFunction("v");
+      break;
+    case 119:
+      console.log("w");
+      keyPressFunction("w");
+      break;
+    case 120:
+      console.log("x");
+      keyPressFunction("x");
+      break;
+    case 121:
+      console.log("y");
+      keyPressFunction("y");
+      break;
+    case 122:
+      console.log("z");
+      keyPressFunction("z");
       break;
 
     default:
